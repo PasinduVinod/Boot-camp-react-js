4e2ce3553ebbd00c027dc2671df4abd0dcd05f1d
PasinduVinod <79078972+PasinduVinod@users.noreply.github.com>
2021-06-29 00:55:57 +0530

    Update Note.jsx

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,1 +1,12 @@
+import React from "react";
 
+function Note(){
+  return{
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>A basic web dev Reaact Js Bootcamp</p>
+    </div>
+  };
+}
+
+export default Note;
